6569ece6e789c1ada89530e6f61731f1b5cd73a1
Ademir Queiroga <ademirneto2007@gmail.com>
2024-06-27 11:11:31 -0300

    add expand animation to score circular indicator

--- common-ui-compose/src/main/java/com/admqueiroga/common/compose/ui/ScoreCircularIndicator.kt
+++ common-ui-compose/src/main/java/com/admqueiroga/common/compose/ui/ScoreCircularIndicator.kt
@@ -1,15 +1,27 @@
 package com.admqueiroga.common.compose.ui
 
 import androidx.annotation.FloatRange
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -38,8 +50,8 @@ import com.admqueiroga.common.compose.theme.LowScoreTrackColor
  */
 @Composable
 fun ScoreCircularIndicator(
-    modifier: Modifier = Modifier,
     @FloatRange(0.0, 1.0) scorePercentage: Float,
+    modifier: Modifier = Modifier,
     trackColor: Color = when (scorePercentage) {
         in 0f..0.4f -> LowScoreTrackColor
         in 0.41f..0.7f -> AverageScoreTrackColor
@@ -53,45 +65,70 @@ fun ScoreCircularIndicator(
 ) {
     val strokeWidth = 2.dp
     val circularProgressModifier = Modifier.padding(2.dp)
+    var expanded by remember { mutableStateOf(false) }
+    val surfaceWidth by animateDpAsState(
+        targetValue = if (expanded) 120.dp else 40.dp,
+        animationSpec = tween(durationMillis = 300)
+    )
     Surface(
-        modifier = modifier.size(48.dp),
+        modifier = modifier
+            .size(width = surfaceWidth, height = 40.dp),
         elevation = 1.dp,
         shape = CircleShape,
         color = MaterialTheme.colors.onSurface
     ) {
-        Box(contentAlignment = Alignment.Center) {
-            // Background track
-            CircularProgressIndicator(
-                modifier = circularProgressModifier,
-                progress = 1f,
-                strokeWidth = strokeWidth,
-                color = trackColor
-            )
-            // Score progress
-            CircularProgressIndicator(
-                modifier = circularProgressModifier,
-                progress = scorePercentage,
-                strokeWidth = strokeWidth,
-                color = progressColor,
-                strokeCap = StrokeCap.Round
-            )
-            Text(
-                text = AnnotatedString.Builder().apply {
-                    append((scorePercentage * 100).toInt().toString())
-                    withStyle(
-                        style = SpanStyle(
-                            color = MaterialTheme.colors.surface.copy(alpha = 0.7f),
-                            fontSize = 8.sp,
-                            baselineShift = BaselineShift(0.35f),
-                        )
-                    ) {
-                        append("%")
-                    }
-                }.toAnnotatedString(),
-                fontSize = 14.sp,
-                fontWeight = FontWeight.Bold,
-                color = MaterialTheme.colors.surface,
-            )
+        Row(
+            modifier = Modifier.clickable {
+                expanded = !expanded
+            }
+        ) {
+            Box(modifier = Modifier.size(40.dp), contentAlignment = Alignment.Center) {
+                // Background track
+                CircularProgressIndicator(
+                    modifier = circularProgressModifier,
+                    progress = 1f,
+                    strokeWidth = strokeWidth,
+                    color = trackColor
+                )
+                // Score progress
+                CircularProgressIndicator(
+                    modifier = circularProgressModifier,
+                    progress = scorePercentage,
+                    strokeWidth = strokeWidth,
+                    color = progressColor,
+                    strokeCap = StrokeCap.Round
+                )
+                Text(
+                    text = AnnotatedString.Builder().apply {
+                        append((scorePercentage * 100).toInt().toString())
+                        withStyle(
+                            style = SpanStyle(
+                                color = MaterialTheme.colors.surface.copy(alpha = 0.7f),
+                                fontSize = 8.sp,
+                                baselineShift = BaselineShift(0.35f),
+                            )
+                        ) {
+                            append("%")
+                        }
+                    }.toAnnotatedString(),
+                    fontSize = 14.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = MaterialTheme.colors.surface,
+                )
+            }
+            if (expanded) {
+                Text(
+                    modifier = Modifier
+                        .align(Alignment.CenterVertically),
+                    text = "123 votes",
+                    fontSize = 14.sp,
+                    maxLines = 1,
+                    fontWeight = FontWeight.Bold,
+                    color = MaterialTheme.colors.surface,
+                )
+            }
+
         }
     }
+
 }
